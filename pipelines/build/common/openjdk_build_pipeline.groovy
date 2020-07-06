5e525c149928e25f8183ff27123eb34144623df5
Will Parker <william.parker1@ibm.com>
2020-07-06 12:14:48 +0100

    debug msg

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -506,11 +506,15 @@ class Build {
             if (cleanWorkspace) {
                 try {
                     if (buildConfig.TARGET_OS == "windows") {
+                        List<String> envVars = buildConfig.toEnvVars()
+                        envVars.add("FILENAME=${filename}" as String)
+                        context.withEnv(envVars) {
                         // Windows machines struggle to clean themselves, see:
                         // https://github.com/AdoptOpenJDK/openjdk-build/issues/1855
                         // https://github.com/AdoptOpenJDK/openjdk-infrastructure/issues/1419
-                        context.sh(script: "echo 'this is the var pwd' $pwd")
+                          context.sh(script: "echo 'this is the var pwd' $workspace")
                         //context.sh(script: "rm -rf ${workspace}/build/src/build/*/jdk/gensrc")
+			}
                         context.cleanWs notFailBuild: true, disableDeferredWipeout: true, deleteDirs: true
                     } else {
                         context.cleanWs notFailBuild: true
